21a113a6c7d44ef887803bb98599d68516aafa29
XiaoYee <503843998@qq.com>
2017-11-09 21:45:14 +0800

    修改CNN，RNN结构
    SPP输入需要padding，网络最开始处也需要zeropadding

--- attention_network.py
+++ attention_network.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.autograd import Variable
+import math
 
 
 
@@ -14,7 +15,7 @@ class SPPLayer(nn.Module):
         self.num_levels = num_levels
         self.pool_type = pool_type
 
-    # need to update......reference:http://www.erogol.com/spp-network-pytorch/  
+    # need to fit arbitrary input size...reference:http://www.erogol.com/spp-network-pytorch/ 
     def forward(self, x):
         bs, c, h, w = x.size()
         pooling_layers = []
@@ -33,34 +34,45 @@ class SPPLayer(nn.Module):
 
 class AttentionNet(nn.Module):
 
-    def __init__(self,spp_level=4):
+    def __init__(self):
         super(AttentionNet, self).__init__()
         # Siamese Architecture
-        self.cnn = nn.Sequential(
-        	nn.Conv2d(5,16,5,stride=1, padding=4),
-        	nn.Tanh(),
-        	nn.MaxPool2d(2,stride=2),
-        	nn.Conv2d(5,32,5,stride=1, padding=4),
-        	nn.Tanh(),
-			nn.MaxPool2d(2,stride=2),
-			nn.Conv2d(5,32,5,stride=1, padding=4),
-			nn.Tanh()
-        	)
-        self.spp_level = spp_level
-        self.spp_layer = SPPLayer(spp_level)
+        self.pad1   = nn.ZeroPad2d((4,4,4,4))
+        self.pad2   = nn.ZeroPad2d((4,4,4,4))
+        self.pad3   = nn.ZeroPad2d((4,4,4,4))
+        self.pad4   = nn.ZeroPad2d((2,2,0,0))
+        self.conv_1 = nn.Conv2d(5,  16, 5,stride=1, padding=1)
+        self.conv_2 = nn.Conv2d(16, 32, 5,stride=1, padding=1)
+        self.conv_3 = nn.Conv2d(32, 32, 5,stride=1, padding=1)
+
+        # self.spp_level = spp_level
+        self.spp_layer = SPPLayer(4)
         self.fc1 = nn.Linear(32*(64+16+4+1), 128)
+
+        # batch_first=True,then the input is provided as (batch,sequence,feature)
         self.rnn = nn.RNN(128,128,batch_first=True,dropout=0.6)
-        self.h_0 = Variable(torch.randn(1, 1, 128))
+        self.h_0 = Variable(torch.randn(1, 1, 128).cuda())
 
 
     def forward(self, x):
 
-    	x = self.cnn(x)
+        # print x.size()   #(16L, 5L, 56L, 40L)
+        x = self.pad1(x)
+        x = F.max_pool2d(F.tanh(self.conv_1(x)), 2, stride=2)
+        x = self.pad2(x)
+        x = F.max_pool2d(F.tanh(self.conv_2(x)), 2, stride=2)
+        x = self.pad3(x)
+        x = F.tanh(self.conv_3(x))
+        # print x.size()   #(16L, 32L, 24L, 20L)
+        # padding for SPP,for 20%8 !=0 
+        x = self.pad4(x)
     	x = self.spp_layer(x)
-    	x = x.view(-1, 32*(64+16+4+1)) 
     	x = F.dropout(x,0.6)   
     	x = self.fc1(x)
-    	x, hn = rnn(x, self.h_0)
+        # print x.size()
+        x = x.view(1,-1,128)
+        print x
+    	x, hn = self.rnn(x, self.h_0)
     	return x
 
 
