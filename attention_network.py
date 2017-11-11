0d432e6a24a945335c0ce8f10323a6b79ad07513
XiaoYee <503843998@qq.com>
2017-11-11 19:59:18 +0800

    搭建完整网络结构
    包括column，row max-pooling，用到欧氏距离

--- attention_network.py
+++ attention_network.py
@@ -3,14 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.autograd import Variable
+import math
 
 
-    # reference:https://discuss.pytorch.org/t/how-to-define-a-new-layer-with-autograd/351
 class MetrixMultiply(nn.Module):
     def __init__(self):
         super(MetrixMultiply, self).__init__()
         self.weight = nn.Parameter(torch.zeros(128,128))
 
+    # reference:https://discuss.pytorch.org/t/how-to-define-a-new-layer-with-autograd/351
     def forward(self, x):
         return x.mm(self.weight)
 
@@ -66,6 +67,7 @@ class AttentionNet(nn.Module):
         self.rnn = nn.RNN(128,128,batch_first=True,dropout=0.6)
         self.h_0 = Variable(torch.randn(1, 1, 128).cuda())
         self.Metrix = MetrixMultiply()
+        self.fc = nn.Linear(128, 150)
 
 
     def forward(self,x,y):
@@ -76,14 +78,38 @@ class AttentionNet(nn.Module):
         y = y.view(1,-1,128)
     	x, hn = self.rnn(x, self.h_0)
         y, hn = self.rnn(y, self.h_0)
-        x = x.squeeze(0)
-        y = y.squeeze(0)
+        P = x.squeeze(0)
+        G = y.squeeze(0)
         #attention begin
-        A = self.Metrix(x)
-        A = F.tanh(A.mm(torch.t(y)))
-        print A.size()
+        A = self.Metrix(P)
+        A = F.tanh(A.mm(torch.t(G)))
+        t_p,indx = torch.max(A,0)
+        t_g,indx = torch.max(A,1)
+        # notice don't view as (16,1),otherwise soft will be all 1
+        t_p = t_p.view(1,16)
+        t_g = t_g.view(1,16)
+        soft = nn.Softmax()
+        a_p = soft(t_p)
+        a_g = soft(t_g)
+        # print a_p.size()
+        v_p = a_p.mm(P)
+        v_g = a_g.mm(G)
+        f_p = self.fc(v_p)
+        f_g = self.fc(v_g)
+        # logsoft = nn.LogSoftmax()
+        # loss_p = logsoft(v_p)
+        # loss_g = logsoft(v_g)
+        # use softmax and cross-entropy loss as paper
+        identity_p = soft(f_p)
+        identity_g = soft(f_g)
 
-    	return A
+        pdist = nn.PairwiseDistance(p=2)
+        distance = pdist(v_p, v_g)
+
+    	return distance,identity_p,identity_g
+
+
+
 
 
 
