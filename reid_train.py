9dff0d204946a2f9dcbfb32b79abc4bb7da51d1d
XiaoYee <503843998@qq.com>
2017-11-21 19:15:07 +0800

    修改训练文件
    删掉一些中间内容

--- reid_train.py
+++ reid_train.py
@@ -10,12 +10,13 @@ import torchvision.models as models
 from torch.autograd import Variable
 import numpy as np
 from attention_network import AttentionNet
+from compute_CMC import computeCMC
 import random
 import cv2
 from dataset import same_pair,different_pair 
 
     
-nEpochs = 2000
+nEpochs = 600
 learning_rate = 0.001
 sampleSeqLength = 16
 
@@ -53,7 +54,7 @@ optimizer = optim.SGD(model.parameters(), lr= learning_rate, momentum=0.9)
 # optimizer = optim.RMSprop(model.parameters(), lr=0.001, alpha=0.9)
 
 def adjust_learning_rate(optimizer, batch):
-    #Sets the learning rate to the initial LR decayed by 10 every 30 epochs
+
     lr = learning_rate
     for i in range(len(steps)):
         scale = scales[i] if i < len(scales) else 1
@@ -68,14 +69,11 @@ def adjust_learning_rate(optimizer, batch):
         param_group['lr'] = lr
     return lr
 
-
-# for parameter in model.parameters():
-#     print(parameter)
-
 loss_log = []
 
 for ep in range(nEpochs):
     # random every epoch
+    model.train()
     loss_add = 0
     order = torch.randperm(nTrainPersons)
     for i in range(nTrainPersons*2):
@@ -93,12 +91,12 @@ for ep in range(nEpochs):
 	        # load data from different identity random
 	        netInputA, netInputB, labelA, labelB ,label_same = different_pair(trainID,sampleSeqLength)
 
-        netInputA = Variable(torch.from_numpy(netInputA).float()).cuda()
-        netInputB = Variable(torch.from_numpy(netInputB).float()).cuda()
+        netInputA = Variable(torch.from_numpy(netInputA.copy()).float()).cuda()
+        netInputB = Variable(torch.from_numpy(netInputB.copy()).float()).cuda()
 
         # optimizer.zero_grad()
 		# v_p,v_g,identity_p,identity_g = model(netInputA,netInputB)
-        distance,identity_p,identity_g = model(netInputA,netInputB)
+        distance,identity_p,identity_g,v_p,v_g = model(netInputA,netInputB)
 
 
         label_same = torch.FloatTensor([label_same])
@@ -116,41 +114,37 @@ for ep in range(nEpochs):
 
         loss = loss_pair+loss_identity1+loss_identity2
 
-        # print "loss:   ",loss
         loss_add = loss_add + loss.data[0]
-        # print loss
-        # Euclidean_distance = (torch.mean(torch.pow((v_p-v_g),2))*(v_p.size()[0])).data.cpu()
-        # zero = torch.FloatTensor([0])
-        # label_same = Variable(label_same)
-        # loss = label_same*Euclidean_distance+(1-label_same)*torch.max(zero, 3-Euclidean_distance)
-        # loss = label_same*Euclidean_distance+(1-label_same)*torch.clamp((3-Euclidean_distance),min=0)
-        # loss need to update
 
-        if i%10 == 0:
-            loss_log.append(loss.data[0])
-            # print('\nepoch: {} - batch: {}/{}'.format(ep, i, len(trainID)*2))
-            # print('loss: ', loss.data[0])
-
-        #### clip gradient parameters to train RNN#####
+        #### clip gradient parameters to train RNN #####
 
         # nn.utils.clip_grad_norm(model.parameters(), 5)
         for p in model.parameters():
             if p.grad is not None:
                 p.grad.data.clamp_(-5, 5)
-
         # torch.nn.utils.clip_grad_norm(model.parameters(),5)
-
         ##############################################
         optimizer.zero_grad()
         loss.backward() 
         optimizer.step()
 
-    if ep%1 ==0:
+    if ep%1 == 0:
         # print('epoch %d, lr %f, loss %f ' % (ep, lr, loss_add))
         print('epoch %d, loss %f ' % (ep , loss_add))
+        loss_log.append(loss_add)
 
+    if (ep+1)%500 == 0:
 
-# print loss_log
+        model.eval()
+        cmc = computeCMC(testID, model)
+        print cmc
+
+    if (ep+1)%600 == 0:
+
+        model.eval()
+        cmc = computeCMC(testID, model)
+        print cmc
+
         
 torch.save(model.state_dict(), './siamese.pth')
 
